78e83b4749f4d903183a723687095d22900c1150
Christian Wolf <cwolf@1-A-29.42heilbronn.de>
2025-07-14 15:33:50 +0200

    finish copy constrcutor+operator+destructor

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: cwolf <cwolf@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 16:52:48 by cwolf             #+#    #+#             */
-/*   Updated: 2025/07/11 17:02:38 by cwolf            ###   ########.fr       */
+/*   Updated: 2025/07/14 14:51:19 by cwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,20 @@ Fixed::Fixed() : _fixedValue(0){
 
 Fixed::Fixed(const Fixed& other)
 {
-	//kopieren???
 	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
 };
 
 Fixed& Fixed::operator=(const Fixed& other)
 {
-	//copy assignend sachen
+	if (this != &other)
+	{
+		this->_fixedValue = other._fixedValue;
+	}
+	return *this;
 };
 
-Fixed::~Fixed(){
-	//delete
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
 };
